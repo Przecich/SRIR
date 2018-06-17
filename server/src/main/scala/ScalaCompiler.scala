96bab1a37e145a8cc93e4878bd92f705356f9291
Karol Domański <ironbrother@vp.pl>
2018-06-17 15:27:01 +0200

    Added clearer compilation messages

--- server/src/main/scala/ScalaCompiler.scala
+++ server/src/main/scala/ScalaCompiler.scala
@@ -8,12 +8,15 @@ object ScalaCompiler {
   private final val nashorn: ScriptEngine = new ScriptEngineManager().getEngineByName("nashorn")
 
   def compile(sourceCode: String): String = {
+    val compilationErrorMsg = "Compilation failed"
+    val compilationSuccessMsg = "Compilation succeeded"
     val result = Try(nashorn.eval(sourceCode))
 
     result match {
-      case Success(null) => "null"
-      case Success(res: ScriptObjectMirror) => res.asScala.toString
-      case Failure(ex) => ex.getMessage
+      case Success(null) => compilationSuccessMsg
+      case Success(res: ScriptObjectMirror) => compilationSuccessMsg + ":\n " + res.asScala.toString
+      case Failure(ex) => compilationErrorMsg + ":\n " + ex.getMessage
+      case _ => compilationErrorMsg
     }
   }
 }
