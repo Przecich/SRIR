1837b96a469b503e3a54403ce93da9e68820194e
P Chromniak <piotr.chromniak@gmail.com>
2018-06-17 18:43:31 +0200

    Added documentation of repository code

--- server/src/main/scala/DifferenceSummary.scala
+++ server/src/main/scala/DifferenceSummary.scala
@@ -1,3 +1,10 @@
+/** Class represents summary of differences of two given codes
+  * @param inserted Inserted characters count
+  * @param deleted Deleted characters count
+  * @param levenshteinDistance Distance between two codes using Levenshtein's metric
+  * */
 class DifferenceSummary(val inserted: Int, val deleted: Int, val levenshteinDistance: Int){
+  /** Calculates absolute difference of codes which is sum of inserted and deleted characters
+    * @return Sum of inserted and deleted characters*/
   def absoluteDifference: Int = inserted + deleted
 }
--- server/src/main/scala/DifferenceUtility.scala
+++ server/src/main/scala/DifferenceUtility.scala
@@ -1,7 +1,12 @@
 import difftool.diff_match_patch
 import scala.collection.JavaConverters._
 
+/** Provides utility to calculate difference between codes and format summary as string */
 object DifferenceUtility {
+  /** Calculates difference between given codes
+    * @param code1 Referential code
+    * @param code2 Code that's being compared to code1
+    * @return Summary of differences between codes*/
   def calculateDifference(code1: String, code2: String): DifferenceSummary = {
     val dmp = new diff_match_patch
     val differenceList = dmp.diff_main(code1, code2)
@@ -19,7 +24,9 @@ object DifferenceUtility {
 
     new DifferenceSummary(addedCount, removedCount, distance)
   }
-
+/** Formats result of the most similar code search to string
+  * @param result Optional tuple of the most similar code and summary of comparison
+  * @return Formatted summary as string*/
   def summaryPrinter(result: Option[(String, DifferenceSummary)]): String = result match {
     case None => "Analiza kodu: Brak kodu w repozytorium"
     case Some((code: String, summary: DifferenceSummary)) => "Analiza kodu:\nDodanych znakow: " + summary.inserted + "\nUsunietych znakow: " + summary.deleted + "\nOdleglosc Levenshteina: " + summary.levenshteinDistance + "\nLoC: " + code.count(_ == '\n').+(1) + "\nCode preview: " + code.take(10) + "..."
